70e55f70c2225a075c22b5d93e1cef2f9a4e470f
Alessandro Mondaini <mondaini.alessandro@gmail.com>
2017-10-26 10:42:58 +0200

    Ajout de jQuery et installation du plugin jquery Smooth Scroll

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import 'bootstrap.native';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './main.css';
+
+import 'jquery-smooth-scroll';
+
+$(document).ready(() => {
+    $('a').smoothScroll();
+});
