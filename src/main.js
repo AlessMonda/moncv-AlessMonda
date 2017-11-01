79512c3abaaec2258fe3b4c68428222657f035a4
Alessandro Mondaini <mondaini.alessandro@gmail.com>
2017-11-01 10:53:28 +0100

    Les barres de progression sont remplacée par des doughnut automatiquement lorsque Javascript est activé sur le navigateur

--- src/main.js
+++ src/main.js
@@ -7,4 +7,25 @@ import Chart from 'chart.js';
 
 $(document).ready(() => {
     $('a').smoothScroll();
+
+    $('.progress').each((index, element) => {
+        let value = $(element.children).attr('aria-valuenow');
+        let remainingValue = $(element.children).attr('aria-valuemax') - value;
+        $(element).replaceWith('<canvas id="chart-' + index + '"></canvas>');
+
+        let ctx = $('#chart-' + index);
+
+            //eslint-disable-next-line
+            new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    datasets: [
+                        {
+                            backgroundColor: ['#3e95cd', '#FFFFFF'],
+                            data: [value, remainingValue]
+                        }
+                    ]
+                }
+            });
+    });
 });
