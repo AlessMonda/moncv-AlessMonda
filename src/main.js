9ba401bb003ad4d4363d3d43237fed5c22f1ca51
Alessandro Mondaini <mondaini.alessandro@gmail.com>
2017-11-08 07:55:26 +0100

    Doughnut Chart désactiver les tooltips lorsque l'on passe sur le chart

--- src/main.js
+++ src/main.js
@@ -25,6 +25,11 @@ $(document).ready(() => {
                             data: [value, remainingValue]
                         }
                     ]
+                },
+                options: {
+                    tooltips: {
+                        enabled: false
+                    }
                 }
             });
     });
